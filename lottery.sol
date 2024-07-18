feb9fad28400aefcef350136f08c3c975310e70d
AKSHITAA9 <2022a1r101@mietjammu.in>
2024-07-18 14:26:01 +0530

    First Code

---
+++ lottery.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.8.22;
+
+contract lottery{
+    struct ticket{
+        string name;
+        uint lotterynumber;
+    }
+
+    mapping(string => uint) public map;
+
+    function store(string memory name, uint lotterynumber ) public{
+        map[]=
+    }
+
+} c
